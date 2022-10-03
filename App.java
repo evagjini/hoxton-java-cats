b94fa67cbc5b14535ece982137ca3ee1779e0ad9
Fevronia Gjini <fevronigjini1@gmail.com>
2022-10-04 00:55:01 +0200

    first commit

---
+++ App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Nera", 1, true);
+        cat1.showCatInfo();
+        Cat cat2 = new Cat("Garfild", 2, false);
+        cat2.showCatInfo();
+    }
+}
